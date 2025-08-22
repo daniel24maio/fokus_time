1e4fa87a74e3d7fb30e192a3e1f503ef48693871
Daniel Silva <daniel24maio@gmail.com>
2025-08-22 17:50:34 -0300

    Versão final após o treinamento. Apenas com um bug com o expo-router.

--- app/index.jsx
+++ app/index.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { ActionButton } from "../components/ActionButton";
 import { FokusButton } from "../components/FokusButton";
+import { IconPause, IconPlay } from "../components/Icons";
 import { Timer } from "../components/Timer";
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
-    image: require('../pomodoro.png'),
+    initialValue: 25 * 60,
+    image: require('./pomodoro.png'),
     display: 'Foco'
   },
   {
 
     id: 'short',
-    initialValue: 5,
-    image: require('../short.png'),
+    initialValue: 5 * 60,
+    image: require('./short.png'),
     display: 'Pausa curta'
   },
   {
 
     id: 'long',
-    initialValue: 15,
-    image: require('../long.png'),
+    initialValue: 15 * 60,
+    image: require('./long.png'),
     display: 'Pausa longa'
   }
 ]
@@ -30,6 +31,45 @@ const pomodoro = [
 export default function Index() {
 
   const [timerType, setTimertype] = useState(pomodoro[0])
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+  const [timerRunning, setTimerRunning] = useState(false)
+
+  const timerRef = useRef(null)
+
+  const clear = () => {
+    if (timerRef.current != null) {
+        clearInterval(timerRef.current)
+    timerRef.current = null    
+    setTimerRunning(false)  
+    }
+  }
+
+  const toogleTimerType = (newTimerType) => {
+    setTimertype(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }
+
+  const toogleTimer = () => {
+      if (timerRef.current){
+        clear ()
+        return
+      }
+
+      setTimerRunning(true)
+
+      const id = setInterval(() => {
+        setSeconds(oldState => {
+          if(oldState === 0){
+           clear()
+           return timerType.initialValue   
+          }           
+
+          return oldState - 1
+        })
+      }, 1000)
+      timerRef.current = id
+  }
 
   return (
     <View
@@ -41,13 +81,17 @@ export default function Index() {
           {pomodoro.map(p => (
             <ActionButton
               key={p.id}
-              onPress={() => setTimertype(p)}
+              onPress={() => toogleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timerType.initialValue} />
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? 'Pausar' : 'Começar'}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          onPress={toogleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
